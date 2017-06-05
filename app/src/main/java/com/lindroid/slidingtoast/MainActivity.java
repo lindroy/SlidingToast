40b59e5433862f3996b2549fcd6aa4a93d3ec369
Lindroid <linyulong2014@163.com>
2017-06-05 14:07:13 +0800

    创建项目

---
+++ app/src/main/java/com/lindroid/slidingtoast/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lindroid.slidingtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void slidingToastClick(){
+
+    }
+}
---
+++ app/src/main/java/com/lindroid/slidingtoast/SlidingToastActivity.java
@@ -0,0 +1,13 @@
+package com.lindroid.slidingtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlidingToastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_toast);
+    }
+}
