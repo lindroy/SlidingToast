1b46196f7e6ef53577ff30608072938b143600b4
Lindroid <linyulong2014@163.com>
2017-06-13 13:44:15 +0800

    增加了一些注释，完成了博客。

--- app/src/main/java/com/lindroid/slidingtoast/CroutonActivity.java
+++ app/src/main/java/com/lindroid/slidingtoast/CroutonActivity.java
@@ -61,29 +61,26 @@ public class CroutonActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            /**1、根布局弹出Crouton**/
             case R.id.btn_root:
-                Crouton.makeText(this,
+                Crouton.makeText(this, //上下文
                         "根布局的Crouton", //Crouton要显示的文字
                         Style.INFO, //Crouton的样式
                         R.id.rl_root) //显示Crouton的布局ID，不写时默认为根布局
                         .show();
                 break;
+            /**2、子布局弹出Crouton**/
             case R.id.btn_child:
                 Crouton.makeText(this, "子布局的Crouton", Style.ALERT, R.id.rl_child).show();
                 break;
+            /**3、自定义Crouton**/
             case R.id.btn_custom:
                 View view = View.inflate(context, R.layout.custom_crouton, null);
-                Crouton crouton = Crouton.make(this, view, R.id.rl_root);
-                crouton.show();
+                Crouton.make(this, view, R.id.rl_root).show();
                 break;
-            case R.id.btn_cfg:
-                Configuration.Builder cfg = new Configuration.Builder();
-                cfg.setInAnimation(R.anim.crouton_in)
-                        .setOutAnimation(R.anim.crouton_out)
-                        .setDuration(1500);
-                Crouton.showText(this, "修改配置后的Crouton", Style.CONFIRM, R.id.rl_root, cfg.build());
-                break;
+            /**4、通过Style.Builder修改背景颜色和字体样式**/
             case R.id.btn_style:
+                //创建Style构造器对象
                 Style.Builder sb = new Style.Builder();
                 sb.setBackgroundColor(android.R.color.black) //背景颜色
                         .setTextColor(android.R.color.holo_orange_light)//字体颜色
@@ -93,8 +90,19 @@ public class CroutonActivity extends AppCompatActivity implements View.OnClickLi
                         .setHeight(120) //Crouton高度
                         .setGravity(Gravity.CENTER) //设置文字居中
                         .setFontName("Ponsi-Regular.otf")//设置字体，直接输入字体名称的字符串
+//                        .setBackgroundDrawable(R.drawable.crouton_pic)
+//                        .setImageScaleType(ImageView.ScaleType.CENTER_CROP);
                 ;
-                Crouton.showText(this, "This is a Crouton", sb.build());
+                Crouton.showText(this, "This is a Crouton", sb.build());//展示图片，文本内容为空字符串即可
+                break;
+            /**5、修改Crouton配置**/
+            case R.id.btn_cfg:
+                //创建Configuration构造器对象
+                Configuration.Builder cfg = new Configuration.Builder();
+                cfg.setInAnimation(R.anim.crouton_in)
+                        .setOutAnimation(R.anim.crouton_out)
+                        .setDuration(1500);
+                Crouton.showText(this, "修改配置后的Crouton", Style.CONFIRM, R.id.rl_root, cfg.build());
                 break;
         }
     }
